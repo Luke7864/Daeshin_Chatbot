ba0778f656e7edfbc5de0d0ba3a94c3842266078
신재욱(Shin Jaeuk) <luke7864@naver.com>
2018-08-21 04:14:04 +0900

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$command = escapeshellcmd('python3 /var/www/html/python/schoolplan.py');
-$rcmd=shell_exec($command);
-$output=file_get_contents('/var/www/html/python/schoolplan.txt');
-$out=(base64_decode($output));
-
-echo $out;
-?>
